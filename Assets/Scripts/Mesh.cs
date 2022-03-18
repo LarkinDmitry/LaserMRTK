b09899bde328b9fb1a60c533ad51a11bc2d596c8
LarkinDmitry <97976764+LarkinDmitry@users.noreply.github.com>
2022-03-18 16:24:20 +0400

    debug

--- Assets/Scripts/Mesh.cs
+++ Assets/Scripts/Mesh.cs
@@ -1,19 +1,22 @@
 using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using Microsoft.MixedReality.Toolkit.XRSDK.OpenXR;
 using UnityEngine;
 
 public class Mesh : MonoBehaviour
 {
     public void HideMesh()
     {
-        var observer = CoreServices.GetSpatialAwarenessSystemDataProvider<IMixedRealitySpatialAwarenessMeshObserver>();
+        //var observer = CoreServices.GetSpatialAwarenessSystemDataProvider<IMixedRealitySpatialAwarenessMeshObserver>();
+        var observer = CoreServices.GetSpatialAwarenessSystemDataProvider<OpenXRSpatialAwarenessMeshObserver>();
 
         observer.DisplayOption = SpatialAwarenessMeshDisplayOptions.None;
     }
 
     public void ShowMesh()
     {
-        var observer = CoreServices.GetSpatialAwarenessSystemDataProvider<IMixedRealitySpatialAwarenessMeshObserver>();
+        //var observer = CoreServices.GetSpatialAwarenessSystemDataProvider<IMixedRealitySpatialAwarenessMeshObserver>();
+        var observer = CoreServices.GetSpatialAwarenessSystemDataProvider<OpenXRSpatialAwarenessMeshObserver>();
 
         observer.DisplayOption = SpatialAwarenessMeshDisplayOptions.Visible;
     }
